e12b132c94a34340cb47343150dd289bd1436d9e
Emanuel <emanunelzsmj@gmail.com>
2023-04-27 19:52:02 -0300

    Criando e usando middlewares

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require ('express')
 
 const app = express();
 
+app.use(express.json());
+
+const log = (req,res,next)=>{
+    console.log(req.body);
+    console.log(`Data: ${Date.now()}`);
+    next();
+}
+
+app.use(log);
+
 app.get('/' , (req , res)=>{
     res.send('<h1>Kevin Spacey is Keyzer Soze !!</h1>');
 })
@@ -10,5 +20,6 @@ app.listen(3000, ()=>{
 })
 
 app.get('/json' , (req,res) =>{
+    console.log(req.body);
     res.json({name : 'KevinSpacey' , isKeyzerSoze : true})
 })
