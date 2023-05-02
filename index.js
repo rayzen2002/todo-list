897540f98a92b706986420dda007c69be27f1d86
Emanuel <emanunelzsmj@gmail.com>
2023-05-02 19:59:00 -0300

    Criando rota na root pra criar pagina com EJS

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require ('express')
-const checklistRouter = require('./src/routes/checklist.js')
-require('./config/database.js')
+const express = require ('express');
+const checklistRouter = require('./src/routes/checklist.js');
+const rootRouter = require('./src/routes/index.js');
+require('./config/database.js');
+const path = require ('path');
+
+
 const app = express();
 app.use(express.json());
 
+app.set('views' , path.join(__dirname , 'src/views'));
+app.set('view engine' , 'ejs');
+
+
+app.use('/', rootRouter)
 app.use('/checklists',checklistRouter)
 
 
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -4,9 +4,14 @@ const router = express.Router();
 
 const Checklist = require('../model/checklist');
 
-router.get('' , (req,res)=>{
-    console.log(`Router do checklist!`)
-    res.send()
+router.get('' ,  async (req,res)=>{
+    try {
+        let checklist = await Checklist.find({});
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(500).json(error)
+        console.log(error)
+    }
 })
 
 router.post('/' , async (req,res)=>{
@@ -25,19 +30,37 @@ router.post('/' , async (req,res)=>{
   
 })
 
-router.get('/:id' , (req,res)=>{
-    console.log(req.params.id);
-    res.send(`ID : ${req.params.id}`);
+router.get('/:id' , async (req,res)=>{
+    try {
+        let checklist = await Checklist.findById(req.params.id)
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+        console.log(error)
+    }
 })
 
-router.put('/:id' , (req,res) =>{
-    console.log(req.params.id);
-    res.send(`PUT ID : ${req.params.id}`);
+router.put('/:id' , async (req,res) =>{
+    // console.log(req.params.id);
+    // res.send(`PUT ID : ${req.params.id}`);
+    try {
+        let { name } = req.body
+        let checklist = await Checklist.findByIdAndUpdate(req.params.id , { "name" : name } , {new : true})
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+        console.log(error)
+    }
 })
 
-router.delete('/:id' , (req,res) =>{
-    console.log(req.params.id);
-    res.send(`DELETE ID : ${req.params.id}`);
+router.delete('/:id' , async (req,res) =>{
+    try {
+        let { name } = req.body
+        let checklist = await Checklist.findByIdAndRemove(req.params.id , { "name" : name } , {new : true})
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+        console.log(error)
+    }
 })
-
 module.exports = router;
